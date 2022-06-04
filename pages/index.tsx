2e236ca13d91b095367526ef606f86bb8a8f0a32
Dmytro Trubin <d.trubin05@gmail.com>
2022-06-04 21:32:36 +0300

    add table example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,6 @@
-import * as React from "react";
-import type { NextPage } from "next";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import Dashboard from "@/components/pages/HomePage/HomePage";
+import React from "react";
 
-const Home: NextPage = () => {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 5,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" color="primary">
-          Material UI v5 with Next.js in TypeScript
-        </Typography>
-        <Typography component="h2" color="secondary">
-          Boilerplate for building faster.
-        </Typography>
-      </Box>
-    </Container>
-  );
-};
+const Home = () => <Dashboard />;
 
 export default Home;
