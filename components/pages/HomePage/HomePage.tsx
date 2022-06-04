4df05a1104e3216c9851f7c6ad12501637db889b
Dmytro Trubin <d.trubin05@gmail.com>
2022-06-05 00:38:57 +0300

    add todo table

--- components/pages/HomePage/HomePage.tsx
+++ components/pages/HomePage/HomePage.tsx
@@ -1,17 +1,13 @@
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Orders from "./components/Orders";
 import { Layout } from "@/components/layouts/Layout";
+import { TodosTable } from "./components/TodosTable";
 
 export const HomePage = () => {
   return (
     <Layout>
       <Grid container spacing={3}>
-        {/* Recent Orders */}
         <Grid item xs={12}>
-          <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-            <Orders />
-          </Paper>
+          <TodosTable />
         </Grid>
       </Grid>
     </Layout>
