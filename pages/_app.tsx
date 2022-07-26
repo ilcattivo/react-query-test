baa9ff84872091ab6beda0a99a5eeb0e9a4e8144
Dmytro Trubin <d.trubin05@gmail.com>
2022-07-26 16:49:47 +0300

    add query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "@/theme/theme";
 import createEmotionCache from "@/theme/createEmotionCache";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -14,6 +15,8 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
+const queryClient = new QueryClient();
+
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
@@ -21,11 +24,13 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </CacheProvider>
   );
 }
