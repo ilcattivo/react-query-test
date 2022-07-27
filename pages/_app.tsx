fba83d1b4844f7868dd7d16ff24f7ace7169449b
Dmytro Trubin <d.trubin05@gmail.com>
2022-07-27 22:37:14 +0300

    add react query devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "@/theme/theme";
 import createEmotionCache from "@/theme/createEmotionCache";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -29,6 +30,7 @@ export default function MyApp(props: MyAppProps) {
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
           <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
         </ThemeProvider>
       </QueryClientProvider>
     </CacheProvider>
