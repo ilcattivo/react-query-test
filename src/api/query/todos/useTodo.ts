a55b02fd0052ce7ab2cf3f60e3bc2f5406d8d7d3
Dmytro Trubin <d.trubin05@gmail.com>
2022-07-28 13:46:07 +0300

    fill todo by initial data

--- src/api/query/todos/useTodo.ts
+++ src/api/query/todos/useTodo.ts
@@ -1,17 +1,25 @@
 import { apiFetcher } from "@/services/ApiService";
 import { Todo } from "@/types/todo";
-import { useQuery } from "@tanstack/react-query";
+import { InfiniteData, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const getTodo = (todoId: number) =>
   apiFetcher<Todo>({
     method: "GET",
-    url: `todo/${todoId}`,
+    url: `todos/${todoId}`,
   });
 
 export const useTodo = (todoId: number) => {
+  const queryClient = useQueryClient();
+
   return useQuery({
     queryKey: ["todos", todoId],
     queryFn: () => getTodo(todoId),
-    keepPreviousData: true,
+    initialData: () => {
+      const allTodos = queryClient.getQueryData(["infinite-todos"]) as
+        | InfiniteData<Todo[]>
+        | undefined;
+
+      return allTodos?.pages.flat().find((todo) => todo.id === todoId);
+    },
   });
 };
