7a0ba42e373dafed51b4d6ced3347a64d3aaf242
Dmytro Trubin <d.trubin05@gmail.com>
2022-07-28 13:03:39 +0300

    change copyright

--- src/components/pages/InfiniteTodoPage/InfiniteTodoPage.tsx
+++ src/components/pages/InfiniteTodoPage/InfiniteTodoPage.tsx
@@ -1,4 +1,5 @@
 import { useTodo } from "@/api/query/todos/useTodo";
+import Title from "@/components/atoms/Title";
 import { Layout } from "@/components/layouts/Layout";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
@@ -8,12 +9,31 @@ export const InfiniteTodoPage = () => {
   const { query } = useRouter();
   const { data: todo, isLoading, isError } = useTodo(Number(query.todoId));
 
+  const renderContent = () => {
+    if (isError) {
+      return <span>Something went wrong</span>;
+    }
+
+    if (isLoading) {
+      return <span>Loading...</span>;
+    }
+
+    return (
+      <div>
+        <Title>
+          Todo: #{todo.id} {todo.title}
+        </Title>
+        <pre>{JSON.stringify(todo, null, 2)}</pre>
+      </div>
+    );
+  };
+
   return (
     <Layout>
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <h2>Todo Id: {query.todoId}</h2>
+            {renderContent()}
           </Grid>
         </Grid>
       </Container>
